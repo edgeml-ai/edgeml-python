0909dc94c72368dd4a516972454a1dc4191e3baa
Sudarshan Bangalore <bsudarshan@outlook.com>
2026-02-08 11:13:09 -0800

    increase test coverage and fix existing tests

---
+++ tests/test_edge.py
@@ -0,0 +1,33 @@
+import unittest
+
+from edgeml.edge import EdgeML
+from edgeml.federated_client import FederatedClient
+
+
+class EdgeMLTests(unittest.TestCase):
+    def test_init(self):
+        client = EdgeML(auth_token_provider=lambda: "token123", org_id="org_1", api_base="https://api.test.com")
+        self.assertEqual(client.org_id, "org_1")
+        self.assertIsNotNone(client.api)
+        self.assertIsNotNone(client.registry)
+        self.assertIsNotNone(client.rollouts)
+        self.assertIsNotNone(client.experiments)
+        self.assertIsNotNone(client.federation)
+
+    def test_client_factory_with_device_identifier(self):
+        edge = EdgeML(auth_token_provider=lambda: "token123", org_id="org_1")
+        fed_client = edge.client(device_identifier="device_123", platform="ios")
+        self.assertIsInstance(fed_client, FederatedClient)
+        self.assertEqual(fed_client.device_identifier, "device_123")
+        self.assertEqual(fed_client.platform, "ios")
+
+    def test_client_factory_without_device_identifier(self):
+        edge = EdgeML(auth_token_provider=lambda: "token123", org_id="org_1")
+        fed_client = edge.client()
+        self.assertIsInstance(fed_client, FederatedClient)
+        self.assertIsNotNone(fed_client.device_identifier)  # Generated automatically
+        self.assertEqual(fed_client.platform, "python")
+
+
+if __name__ == "__main__":
+    unittest.main()
